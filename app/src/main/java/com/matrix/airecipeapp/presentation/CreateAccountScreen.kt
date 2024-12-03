04d096964f0b09979a548c60211458c04661eb3f
Sarmesh Kalwar <sarmesh17@gmail.com>
2024-12-03 15:10:05 +0545

    [update]: code formatted

--- app/src/main/java/com/matrix/airecipeapp/presentation/CreateAccountScreen.kt
+++ app/src/main/java/com/matrix/airecipeapp/presentation/CreateAccountScreen.kt
@@ -43,93 +43,158 @@ import com.matrix.airecipeapp.R
 
 @Composable
 @Preview(showBackground = true)
-fun CreateAccountScreen(){
-    var email by remember {androidx.compose.runtime.mutableStateOf("")
+fun CreateAccountScreen() {
+    var email by remember {
+        androidx.compose.runtime.mutableStateOf("")
 
     }
-    var password by remember {androidx.compose.runtime.mutableStateOf("")
+    var password by remember {
+        androidx.compose.runtime.mutableStateOf("")
 
     }
-    var uesrname by remember {androidx.compose.runtime.mutableStateOf("")
+    var uesrname by remember {
+        androidx.compose.runtime.mutableStateOf("")
 
     }
-    Column (modifier = Modifier.fillMaxSize()){
+    Column(modifier = Modifier.fillMaxSize()) {
         Box {
-            Image(painter = painterResource(id = R.drawable.imgofcreateacc), contentDescription =null, modifier = Modifier
-                .fillMaxWidth()
-                .height(260.dp) )
-            Row (modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 20.dp, end = 10.dp, start = 10.dp, bottom = 10.dp), horizontalArrangement = Arrangement.SpaceBetween){
+            Image(
+                painter = painterResource(id = R.drawable.imgofcreateacc),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(260.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp, end = 10.dp, start = 10.dp, bottom = 10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
 
 
                 Icon(
                     painter = painterResource(id = R.drawable.vector),
                     contentDescription = null, tint = Color.White,
-                    modifier = Modifier.size(20.dp))
+                    modifier = Modifier.size(20.dp)
+                )
                 Text(text = "Log In", fontSize = 20.sp, color = Color.White)
             }
         }
-        Row (modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 20.dp, start = 15.dp)){
-            Text(text = "Let's start making good meals", fontSize =40.sp, color = colorResource(id = R.color.amber))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 20.dp, start = 15.dp)
+        ) {
+            Text(
+                text = "Let's start making good meals",
+                fontSize = 40.sp,
+                color = colorResource(id = R.color.amber)
+            )
 
         }
         Column {
 
-            TextField(value = uesrname, onValueChange = {uesrname = it}, placeholder = { Text(text = "User Name", fontSize = 15.sp)},
-                colors = TextFieldDefaults.colors(unfocusedIndicatorColor = colorResource(
-                    id = R.color.amber), focusedIndicatorColor = colorResource(id = R.color.amber),
-                    focusedContainerColor = Color.White, unfocusedContainerColor = Color.White), modifier = Modifier
+            TextField(value = uesrname,
+                onValueChange = { uesrname = it },
+                placeholder = { Text(text = "User Name", fontSize = 15.sp) },
+                colors = TextFieldDefaults.colors(
+                    unfocusedIndicatorColor = colorResource(
+                        id = R.color.amber
+                    ), focusedIndicatorColor = colorResource(id = R.color.amber),
+                    focusedContainerColor = Color.White, unfocusedContainerColor = Color.White
+                ),
+                modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 10.dp, bottom = 10.dp, start = 25.dp, end = 25.dp))
+                    .padding(top = 10.dp, bottom = 10.dp, start = 25.dp, end = 25.dp)
+            )
             Spacer(modifier = Modifier.height(10.dp))
-            TextField(value = email, onValueChange = {email = it}, placeholder = { Text(text = "Your Email", fontSize = 15.sp)},
-                colors = TextFieldDefaults.colors(unfocusedIndicatorColor = colorResource(
-                id = R.color.amber), focusedIndicatorColor = colorResource(id = R.color.amber),
-                focusedContainerColor = Color.White, unfocusedContainerColor = Color.White), modifier = Modifier
+            TextField(value = email,
+                onValueChange = { email = it },
+                placeholder = { Text(text = "Your Email", fontSize = 15.sp) },
+                colors = TextFieldDefaults.colors(
+                    unfocusedIndicatorColor = colorResource(
+                        id = R.color.amber
+                    ), focusedIndicatorColor = colorResource(id = R.color.amber),
+                    focusedContainerColor = Color.White, unfocusedContainerColor = Color.White
+                ),
+                modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 10.dp, bottom = 10.dp, start = 25.dp, end = 25.dp))
+                    .padding(top = 10.dp, bottom = 10.dp, start = 25.dp, end = 25.dp)
+            )
             Spacer(modifier = Modifier.height(10.dp))
-            TextField(value = password, onValueChange = {password = it}, placeholder = { Text(text = "Password", fontSize = 15.sp)},
-                colors = TextFieldDefaults.colors(unfocusedIndicatorColor = colorResource(
-                    id = R.color.amber), focusedIndicatorColor = colorResource(id = R.color.amber),
-                    focusedContainerColor = Color.White, unfocusedContainerColor = Color.White), modifier = Modifier
+            TextField(value = password,
+                onValueChange = { password = it },
+                placeholder = { Text(text = "Password", fontSize = 15.sp) },
+                colors = TextFieldDefaults.colors(
+                    unfocusedIndicatorColor = colorResource(
+                        id = R.color.amber
+                    ), focusedIndicatorColor = colorResource(id = R.color.amber),
+                    focusedContainerColor = Color.White, unfocusedContainerColor = Color.White
+                ),
+                modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 10.dp, bottom = 10.dp, start = 25.dp, end = 25.dp))
+                    .padding(top = 10.dp, bottom = 10.dp, start = 25.dp, end = 25.dp)
+            )
 
         }
         Spacer(modifier = Modifier.height(15.dp))
-        Column (modifier = Modifier
-            .height(250.dp)
-            .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally)
+        Column(
+            modifier = Modifier
+                .height(250.dp)
+                .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+        )
         {
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(234.dp,40.dp), colors = ButtonDefaults.buttonColors(contentColor = colorResource(
-                id = R.color.black), containerColor = colorResource(id = R.color.amber)), shape = RoundedCornerShape(10.dp) ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(234.dp, 40.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = colorResource(
+                        id = R.color.black
+                    ), containerColor = colorResource(id = R.color.amber)
+                ),
+                shape = RoundedCornerShape(10.dp)
+            ) {
                 Text(text = "Create Account")
             }
             Spacer(modifier = Modifier.height(15.dp))
             Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                HorizontalDivider(thickness = 1.5.dp, color = Color.Black, modifier = Modifier
-                    .width(80.dp)
-                    .padding(top = 8.dp))
+                HorizontalDivider(
+                    thickness = 1.5.dp, color = Color.Black, modifier = Modifier
+                        .width(80.dp)
+                        .padding(top = 8.dp)
+                )
                 Spacer(modifier = Modifier.width(8.dp))
-                Text(text ="OR")
+                Text(text = "OR")
                 Spacer(modifier = Modifier.width(8.dp))
 
-                HorizontalDivider(thickness = 1.5.dp, color = Color.Black, modifier = Modifier
-                    .width(80.dp)
-                    .padding(top = 8.dp))
-                
+                HorizontalDivider(
+                    thickness = 1.5.dp, color = Color.Black, modifier = Modifier
+                        .width(80.dp)
+                        .padding(top = 8.dp)
+                )
+
             }
             Spacer(modifier = Modifier.height(10.dp))
 
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(234.dp,40.dp), colors = ButtonDefaults.buttonColors(contentColor = colorResource(
-                id = R.color.black), containerColor = colorResource(id = R.color.white)), shape = RoundedCornerShape(10.dp), border = BorderStroke(width = 1.dp, color = Color.Black) ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(234.dp, 40.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = colorResource(
+                        id = R.color.black
+                    ), containerColor = colorResource(id = R.color.white)
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(width = 1.dp, color = Color.Black)
+            ) {
                 Row {
-                    Icon(painter = painterResource(id = R.drawable.facebook), contentDescription = null, modifier = Modifier.size(20.dp),
-                        Color.Gray)
+                    Icon(
+                        painter = painterResource(id = R.drawable.facebook),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp),
+                        Color.Gray
+                    )
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(text = "Sign Up with Facebook", fontSize = 15.sp)
 
@@ -138,11 +203,24 @@ fun CreateAccountScreen(){
             }
             Spacer(modifier = Modifier.height(10.dp))
 
-            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(234.dp,40.dp), colors = ButtonDefaults.buttonColors(contentColor = colorResource(
-                id = R.color.black), containerColor = colorResource(id = R.color.white)), shape = RoundedCornerShape(10.dp), border = BorderStroke(width = 1.dp, color = Color.Black) ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.size(234.dp, 40.dp),
+                colors = ButtonDefaults.buttonColors(
+                    contentColor = colorResource(
+                        id = R.color.black
+                    ), containerColor = colorResource(id = R.color.white)
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(width = 1.dp, color = Color.Black)
+            ) {
                 Row {
-                    Icon(painter = painterResource(id = R.drawable.google), contentDescription = null, modifier = Modifier.size(20.dp),
-                        Color.Gray)
+                    Icon(
+                        painter = painterResource(id = R.drawable.google),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp),
+                        Color.Gray
+                    )
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(text = "Sign Up with Google", fontSize = 15.sp)
 
@@ -150,11 +228,10 @@ fun CreateAccountScreen(){
 
             }
             Spacer(modifier = Modifier.height(10.dp))
-Text(text = "Term of Use and Privacy Policy", fontSize = 13.sp)
-            }
-            
-
-
+            Text(text = "Term of Use and Privacy Policy", fontSize = 13.sp)
         }
+
+
     }
+}
 
