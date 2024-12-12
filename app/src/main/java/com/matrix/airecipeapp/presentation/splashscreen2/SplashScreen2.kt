7a162d6651a66c4e7d22d5c1dc8768d1d5ba01cd
National <gkrisha66@gmail.com>
2024-12-12 22:37:39 +0545

    ntg

--- app/src/main/java/com/matrix/airecipeapp/presentation/splashscreen2/SplashScreen2.kt
+++ app/src/main/java/com/matrix/airecipeapp/presentation/splashscreen2/SplashScreen2.kt
@@ -1,2 +1,10 @@
 package com.matrix.airecipeapp.presentation.splashscreen2
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview(showBackground = true)
+fun SplashScreen2(){
+
+}
