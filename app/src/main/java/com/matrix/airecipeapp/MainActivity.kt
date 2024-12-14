c70765231eb6ed7a731eaaab84a12f0e0b58749d
Sarmesh Kalwar <sarmesh17@gmail.com>
2024-12-14 12:32:00 +0545

    [fixed]:compact screen size

--- app/src/main/java/com/matrix/airecipeapp/MainActivity.kt
+++ app/src/main/java/com/matrix/airecipeapp/MainActivity.kt
@@ -12,20 +12,20 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.matrix.airecipeapp.presentation.navigation.AiRecipeNavigationSystem
 import com.matrix.airecipeapp.presentation.viewmodels.AuthViewModel
 import com.matrix.airecipeapp.ui.theme.AIRecipeAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val authViewModel:AuthViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AIRecipeAppTheme {
 
-//                CreateAccountScreen(authViewModel)
 
+                AiRecipeNavigationSystem()
 
             }
         }
--- app/src/main/java/com/matrix/airecipeapp/presentation/navigation/AIIRecipeAppNavigationSystem.kt
+++ app/src/main/java/com/matrix/airecipeapp/presentation/navigation/AIIRecipeAppNavigationSystem.kt
@@ -5,18 +5,37 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.matrix.airecipeapp.WindowType
+import com.matrix.airecipeapp.presentation.createaccountscreen.CompactCreateAccountScreen
+import com.matrix.airecipeapp.presentation.createaccountscreen.MediumToExpandedCreateAccountScreen
 import com.matrix.airecipeapp.presentation.viewmodels.AuthViewModel
+import com.matrix.airecipeapp.rememberWindowSize
 
 @Composable
 fun AiRecipeNavigationSystem(){
 
     val navController= rememberNavController()
-
+     val windowSize = rememberWindowSize()
     NavHost(startDestination = Routes.CreateAccountScreen , navController = navController ) {
 
         composable<Routes.CreateAccountScreen> {
 
             val authViewModel:AuthViewModel= hiltViewModel()
+
+            when(windowSize.width){
+
+                WindowType.Compact -> {
+
+                    CompactCreateAccountScreen(authViewModel)
+                }
+
+                else -> {
+                    MediumToExpandedCreateAccountScreen(authViewModel)
+
+                }
+            }
+
+
 
         }
 
